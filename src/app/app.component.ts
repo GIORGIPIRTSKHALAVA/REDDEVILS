d3902cef84c642071a87fe6768ad384546b70ab4
GIORGIPIRTSKHALAVA <pircxalava.giorgi2004@gmail.com>
2024-04-12 00:25:48 +0400

    new added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
-import { Component, } from '@angular/core';
+
+import { Component } from '@angular/core';
 import { CounterComponent } from './counter/counter.component';
 import { RouterOutlet } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
 
+export interface User {
+  name: string;
+  email: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    CounterComponent
-  ],
-  
+  imports: [RouterOutlet, CounterComponent, UserListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  users: User[] = [
+    { name: 'John Doe', email: 'john.doe@example.com' },
+    { name: 'Jane Doe', email: 'jane.doe@example.com' },
+    { name: 'Alice Smith', email: 'alice.smith@example.com' },
+    { name: 'Bob Smith', email: 'bob.smith@example.com' },
+  ];
+
   initialValue: number = 0;
   title = 'angular';
 
-
   onValueChange(event: number) {
     console.log('New value:', event);
-
   }
-} 
- 
-
+}
